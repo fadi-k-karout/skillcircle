77f941498a73376eff0dfa9e4c37dca8f2ade944
fadi-k-karout <97478111+fadi-k-karout@users.noreply.github.com>
2024-11-11 00:17:09 +0300

    chore: remove unused imports

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,7 +1,6 @@
 // src/Routes.tsx
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import PrivateRoute from '../components/PrivateRoute';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
 import Registration from '../pages/Registration';
